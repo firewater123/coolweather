084ccae7c76a2bbd7c670c4ff87940f59a300d2c
firewater <382145385@qq.com>
2017-05-28 18:09:37 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package android.coolweather.com.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/5/22 0022.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
